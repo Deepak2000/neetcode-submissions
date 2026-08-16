9f441b2f111a4a9c83f7bf2f46e1f49fa0f0ac71
Deepak Joshi <dj1676106@gmail.com>
2026-08-16 20:25:58 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.ts
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        const buckets = new Array<Array<number>>(nums.length);
+        const freqMap = new Map<number, number>();
+
+        for(const num of nums){
+            if(freqMap.has(num)){
+                const newFreq = freqMap.get(num) + 1;
+                freqMap.set(num, newFreq);
+            }else{
+                freqMap.set(num, 1);
+            }
+        }
+
+        for(const [num, freq] of freqMap){
+            if(buckets[freq]){
+                buckets[freq].push(num);
+            }else{
+                buckets[freq] = [num];
+            }
+        }
+
+        let topFreqCount = 0;
+        const res:Array<number> = [];
+        for(let i=buckets.length-1; i>=0; i--){
+            if(!!buckets[i]){
+                for(const num of buckets[i]){
+                    if(topFreqCount < k){
+                        res.push(num);
+                        topFreqCount++;
+                    }
+                }
+            }
+            if(topFreqCount == k) break;
+        }
+        // console.log(buckets, freqMap, topFreqCount, res)
+        return res;
+    }
+}
