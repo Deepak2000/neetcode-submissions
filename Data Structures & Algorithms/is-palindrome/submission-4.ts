8674a06f07cc6d576dffef5a336598bd865fda82
Deepak Joshi <dj1676106@gmail.com>
2026-08-17 15:32:02 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.ts
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        
+        let sanitized = "";
+        for(let c of s){
+            if(this.isAlphaNumeric(c))
+            sanitized += c.toLowerCase();
+        }
+        let lPtr:number=0, rPtr:number=sanitized.length-1;
+        while(lPtr<=rPtr){
+            if(sanitized[lPtr] == sanitized[rPtr]){
+                lPtr++;
+                rPtr--;
+            }else{
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    isAlphaNumeric(c:string): boolean{
+        return(
+            (c.charCodeAt(0) >= 'a'.charCodeAt(0) && c.charCodeAt(0) <= 'z'.charCodeAt(0)) ||
+            (c.charCodeAt(0) >= 'A'.charCodeAt(0) && c.charCodeAt(0) <= 'Z'.charCodeAt(0)) ||
+            (c.charCodeAt(0) >= '0'.charCodeAt(0) && c.charCodeAt(0) <= '9'.charCodeAt(0))
+        )
+    }
+}
